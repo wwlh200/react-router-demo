46252a72de8a38c1e9ba8212ba2f68cf0765b668
wangwl04 <wangwl04@mingyuanyun.com>
2020-05-15 10:50:00 +0800

    完成demo

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,13 +1,30 @@
 import * as React from "react";
 import RouteView, { IRouteViewProps } from "../routes/RouteView";
+import { History } from "history";
 
 interface ILayoutProps extends IRouteViewProps {
+  history: History;
 }
 
 const Layout = (props: ILayoutProps) => {
+  const handleClick = React.useCallback((e) => {
+    const { name } = e.target;
+    props.history.push(name);
+  }, [props.history]);
+
   return (
     <div>
-      <div>我是Layout</div>
+      <div>
+        <button name="/basic/page1" onClick={handleClick}>
+          Page1
+        </button>
+        <button name="/basic/page2" onClick={handleClick}>
+          Page2
+        </button>
+        <button name="/basic/page3" onClick={handleClick}>
+          Page3
+        </button>
+      </div>
       <RouteView {...props} />
     </div>
   );
